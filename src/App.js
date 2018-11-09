2d7bfdedfa45698748d51c338316c039c7f2fe6e
kriss <kriss.heimanis@westpac.com.au>
2018-11-09 15:58:18 +1100

    added all url iteration pieces

--- src/App.js
+++ src/App.js
@@ -1,28 +1,88 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import 'style/gui.css';
+import React, { Component } from 'react'
+// import './App.css';
+import 'style/gui.css'
+
+// Components
+import ControlledRow from 'components/ControlledRow'
+
+// feeds
+import contentSnippetsJson from 'feed/snippets'
+import contentSummariesJson from 'feed/summaries'
 
 class App extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            stateSummaries: contentSummariesJson,
+            stateSnippets: contentSnippetsJson
+        }
+
+        this.handleSummaryChange = this.handleSummaryChange.bind(this)
+        this.handleSnippetChange = this.handleSnippetChange.bind(this)
+
+    }
+
+    handleSummaryChange = (e) => {
+        let stateSummaries = this.state.stateSummaries
+        stateSummaries[e.target.id].checked = !stateSummaries[e.target.id].checked
+        this.setState({
+            stateSummaries: stateSummaries
+        })
+    }
+
+    handleSnippetChange = (e) => {
+        let stateSnippets = this.state.stateSnippets
+        stateSnippets[e.target.id].checked = !stateSnippets[e.target.id].checked
+        this.setState({
+            stateSnippets: stateSnippets
+        })
+    }
+
     render() {
         return (
             <div className="App">
                 <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-          </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-          </a>
+                    CAI URL/Brief Generator
                 </header>
+                <main>
+                    <div className='section summary'>
+                        <h2>Summary</h2>
+                        {
+                            this.state.stateSummaries.map((row, i) => (
+                                <ControlledRow
+                                    key={i}
+                                    id={i}
+                                    rowContent={{
+                                        "title": row.intent,
+                                        "content": row.content
+                                    }}
+                                    rowChecked={row.checked}
+                                    controlFunc={this.handleSummaryChange}
+                                />
+                            ))
+                        }
+                    </div>
+                    <div className='section snippet'>
+                        <h2>Snippets</h2>
+                        {
+                            this.state.stateSnippets.map((row, i) => (
+                                <ControlledRow
+                                    key={i}
+                                    id={i}
+                                    rowContent={{
+                                        "title": row.title,
+                                        "content": row.content
+                                    }}
+                                    rowChecked={row.checked}
+                                    controlFunc={this.handleSnippetChange}
+                                />
+                            ))
+                        }
+                    </div>
+                </main>
             </div>
-        );
+        )
     }
 }
 
